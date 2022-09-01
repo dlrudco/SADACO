72d67f20670e2dae776097ef29921adc869e7b8f
dlrudco <kyungchae123@gmail.com>
2022-09-01 12:14:30 +0900

    contrastive in progress

--- apis/contrastive/trainer_contrastive.py
+++ apis/contrastive/trainer_contrastive.py
@@ -6,4 +6,6 @@ class ContrastTrainer(BaseTrainer):
         super().__init__(train_configs)
         
     def attach_extractor(self):
-        target_layer = self.configs.train.
+        target_layer = self.model_configs.contrastive_layer
+        
+        self.mapper = 
